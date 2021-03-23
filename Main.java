fd9c10b6d5c8d95cf42f43e335bd3f5320123de0
Kyle McDowell <kymcd21@icstudents.org>
2021-03-23 17:25:21 +0000

    nice

--- Main.java
+++ Main.java
@@ -1,19 +1,20 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 import java.lang.*;
 
 /**
-   This program demonstrates the permutation generator.
-*/
-public class Main
-{
-   public static void main(String[] args)
-   {
-      PermutationGenerator generator = new PermutationGenerator("big");
-      ArrayList<String> permutations = generator.getPermutations();
-      for (String s : permutations)
-      {         
-         System.out.println(s);
-      }
-   }
+ * This program demonstrates the permutation generator.
+ */
+public class Main {
+  public static void main(String[] args) {
+    Scanner seFodeu = new Scanner(System.in);
+    System.out.println("enter word 4 permutation thing : ");
+    String morre = seFodeu.next();
+    PermutationGenerator generator = new PermutationGenerator(morre);
+    ArrayList<String> permutations = generator.getPermutations();
+    System.out.println(permutations.size());
+    for (String s : permutations) {
+      System.out.println(s);
+    }
+  }
 }
-
--- PermutationGenerator.java
+++ PermutationGenerator.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 public class PermutationGenerator
 {
    private String word;
+   int permutationCounter=0;
 
    /**
       Constructs a permutation generator.
